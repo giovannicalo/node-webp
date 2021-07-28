9dae2902a4cd8ca9bdef070473f24c32ff1b94cf
Giovanni Calò <giovanni.calo@live.it>
2021-07-28 23:14:27 +0100

    Added decode module

---
+++ source/native/decode.cpp
@@ -0,0 +1,31 @@
+#include "decode.hpp"
+
+namespace nodeWebp {
+
+	Napi::Value decode(const Napi::CallbackInfo& info) {
+		if (!info.Length()) {
+			Napi::Error::New(
+				info.Env(),
+				"[nodeWebp::decode] At least one argument is required"
+			).ThrowAsJavaScriptException();
+			return info.Env().Null();
+		}
+		if (!info[0].IsBuffer()) {
+			Napi::Error::New(
+				info.Env(),
+				"[nodeWebp::decode] Argument is not a buffer"
+			).ThrowAsJavaScriptException();
+			return info.Env().Null();
+		}
+		Decoder* decoder = new Decoder(
+			info.Env(),
+			info[0].As<Napi::Uint8Array>(),
+			info.Length() >= 2 ? static_cast<Format>(
+				info[1].As<Napi::Number>().Int32Value()
+			) : Format::rgba
+		);
+		decoder->Queue();
+		return decoder->getPromise();
+	}
+
+}
