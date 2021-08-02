5209d859aa5976149cc3b686e805053a15f005d2
Giovanni Calò <giovanni.calo@live.it>
2021-08-02 04:19:24 +0100

    Added benchmark script

---
+++ scripts/benchmark.js
@@ -0,0 +1,35 @@
+const { readFileSync } = require("fs");
+const { join } = require("path");
+
+const { Format, decode, encode } = require("../source");
+
+const iterations = 1000;
+
+const path = join(__dirname, "..", "test");
+
+const webp = readFileSync(join(path, "512x512.webp"));
+
+const rgba = {
+	data: readFileSync(join(path, "512x512.rgba")),
+	format: Format.rgba,
+	height: 512,
+	width: 512
+};
+
+const benchmark = async (label, method, ...parameters) => {
+	const start = Date.now();
+	for (let i = 0; i < iterations; i++) {
+		await method(...parameters); // eslint-disable-line no-await-in-loop
+	}
+	console.log(`${label}: ${((Date.now() - start) / iterations).toFixed(2)}ms`);
+};
+
+(async () => {
+	for (const parameters of [
+		["WebP => RGBA", decode, webp, Format.rgba],
+		["WebP => YUV ", decode, webp, Format.yuv],
+		["RGBA => WebP", encode, rgba, 90]
+	]) {
+		await benchmark(...parameters); // eslint-disable-line no-await-in-loop
+	}
+})();
