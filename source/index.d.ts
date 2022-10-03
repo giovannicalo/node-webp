c118d7ec7889dc9a8b499e851bb4cfd815398dd7
Giovanni Calò <giovanni.calo@live.it>
2022-10-03 01:03:13 +0100

    Improved types

--- source/index.d.ts
+++ source/index.d.ts
@@ -1,26 +1,38 @@
-export enum Format { rgba, yuv }
+declare namespace webp {
 
-interface Image {
-	data: Uint8ClampedArray;
-	format: Format;
-	height: number;
-	width: number;
+	/**
+	 * @typedef Format
+	 */
+	enum Format { rgba, yuv }
+
+	/**
+	 * @typedef Image
+	 */
+	interface Image {
+		data: Uint8ClampedArray;
+		format: Format;
+		height: number;
+		width: number;
+	}
+
+	/**
+	 * Decodes a WebP image.
+	 *
+	 * @param {Buffer} data - A WebP-encoded buffer.
+	 * @param {Format} [format=Format.rgba] - The desired output format, either RGBA or YUV I420, defaults to RGBA.
+	 * @returns {Promise<Image>} A promise resolving to the decoded image.
+	 */
+	function decode(data: Buffer, format: Format): Promise<Image>;
+
+	/**
+	 * Encodes a raw image as WebP.
+	 *
+	 * @param {Image} image - An image.
+	 * @param {Number} [quality=90] - The desired output quality, between 0 and 100, defaults to 90.
+	 * @returns {Promise<Buffer>} A promise resolving to the encoded buffer.
+	 */
+	function encode(image: Image, quality: number): Promise<Buffer>;
+
 }
 
-/**
- * Decodes a WebP image.
- *
- * @param {Buffer} data - A WebP-encoded buffer.
- * @param {Format} [format=Format.rgba] - The desired output format, either RGBA or YUV I420, defaults to RGBA.
- * @returns {Promise<Image>} A promise resolving to the decoded image.
- */
-export function decode(data: Buffer, format = Format.rgba): Promise<Image>
-
-/**
- * Encodes a raw image as WebP.
- *
- * @param {Image} image - An image.
- * @param {Number} [quality=90] - The desired output quality, between 0 and 100, defaults to 90.
- * @returns {Promise<Buffer>} A promise resolving to the encoded buffer.
- */
-export function encode(image: Image, quality = 90): Promise<Buffer>
+export = webp;
